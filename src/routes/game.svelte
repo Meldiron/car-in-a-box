3baf78e48599163fde671d5227889237d50d3e0f
Matej Bačo <matejbaco2000@gmail.com>
2022-06-28 16:33:39 +0000

    Finished base of game logic

--- src/routes/game.svelte
+++ src/routes/game.svelte
@@ -9,118 +9,185 @@
 			root: document.getElementById('game') as HTMLDivElement
 		});
 
-		// TODO: Put into "game" scene
-		// TODO: Make game over
-
-		// Render background
-		const bgSprite = await loadSprite('bg', '/background_brown.png');
-		const bg = add([
-			sprite('bg', { tiled: true, width: g.width(), height: g.height() }),
-			pos(0, 0),
-			fixed()
-		]);
-
-		// Render walls
-		const walls = [
+		const gameOverScene = scene("gameOver", () => {
 			add([
-				pos(10, 10),
-				'wall',
-				rect(g.width() - 28, 8),
-				color(Color.fromArray([148, 89, 36])),
-				area(),
-				body(),
-				fixed()
-			]),
-			add([
-				pos(g.width() - 10, 10),
-				g.origin('topright'),
-				'wall',
-				rect(8, g.height() - 20),
-				color(Color.fromArray([148, 89, 36])),
-				area(),
-				body(),
-				fixed()
-			]),
-			add([
-				pos(10, g.height() - 10),
-				'wall',
-				g.origin('botleft'),
-				rect(g.width() - 28, 8),
-				color(Color.fromArray([148, 89, 36])),
-				area(),
-				body(),
-				fixed()
-			]),
-			add([
-				pos(10, 10),
-				'wall',
-				g.origin('topleft'),
-				rect(8, g.height() - 20),
-				color(Color.fromArray([148, 89, 36])),
-				area(),
-				body(),
-				fixed()
-			])
-		];
-
-		// Render player
-		const carSprite = await loadSprite('car', '/car.png');
-		const car = add([
-			sprite('car'),
-			pos(center()),
-			area({
-				width: 61,
-				shape: 'circle',
-				height: 36
-			}),
-			gravity(0),
-			body(),
-			'player',
-			{
-				dir: UP,
-				speed: 200
-			},
-			rotate(0),
-			g.origin('center')
-		]);
-
-		// Player movement + controls
-		let isRotating = false;
-		let angleAcceleration = 1;
-		let angleForce = 0.05;
-
-		const setRotating = (state: boolean) => {
-			isRotating = state;
-
-			if (!state) {
-				angleAcceleration = 1;
-				onPickupCoin();
-			}
-		};
-
-		onTouchStart(() => setRotating(true));
-		onMouseDown(() => setRotating(true));
-
-		onTouchEnd(() => setRotating(false));
-		onMouseRelease(() => setRotating(false));
-
-		car.onDraw(() => {
-			console.log(g.debug.fps());
-			car.move(g.Vec2.fromAngle(car.angle).scale(car.speed));
-
-			if (isRotating) {
-				car.angle += Math.min(angleAcceleration, 10);
-				angleAcceleration += angleForce;
-			}
+				text("GAME OVER ", {size: 40, font: "sink"}),
+				pos(center()),
+				g.origin("center"),
+				layer("ui"),
+			]);
 		});
 
-		// Coin spawning
+		const gameScene = scene("game", async () => {
+			// Render background
+			const bgSprite = await loadSprite('bg', '/background_brown.png');
+			const bg = add([
+				sprite('bg', { tiled: true, width: g.width(), height: g.height() }),
+				pos(0, 0),
+				fixed()
+			]);
 
-		// Coin functionality
-		const onPickupCoin = () => {
-			car.speed += 20;
-			angleForce += 0.005;
-		};
+			// Render walls
+			const walls = [
+				add([ // Top
+					pos(18, 10),
+					'wall',
+					rect(g.width() - 36, 8),
+					color(Color.fromArray([148, 89, 36])),
+					area(),
+					body(),
+					gravity(0),
+					fixed()
+				]),
+				add([ // Right
+					pos(g.width() - 10, 10),
+					g.origin('topright'),
+					'wall',
+					rect(8, g.height() - 20),
+					color(Color.fromArray([148, 89, 36])),
+					area(),
+					body(),
+					gravity(0),
+					fixed()
+				]),
+				add([ // Bottom
+					pos(18, g.height() - 10),
+					'wall',
+					g.origin('botleft'),
+					rect(g.width() - 36, 8),
+					color(Color.fromArray([148, 89, 36])),
+					area(),
+					body(),
+					gravity(0),
+					fixed()
+				]),
+				add([ // Left
+					pos(10, 10),
+					'wall',
+					g.origin('topleft'),
+					rect(8, g.height() - 20),
+					color(Color.fromArray([148, 89, 36])),
+					area(),
+					body(),
+					fixed(),
+					gravity(0),
+				])
+			];
+
+			// Wall functionality
+			onCollide("player", "wall", (player, wall) => {
+				go("gameOver");
+			});
+
+			// Render player
+			const carSprite = await loadSprite('car', '/car.png');
+			const car = add([
+				sprite('car'),
+				pos(center()),
+				area(),
+				gravity(0),
+				body(),
+				'player',
+				{
+					dir: UP,
+					speed: 200
+				},
+				rotate(0),
+				g.origin('center')
+			]);
+
+			// Player movement + controls
+			let isRotating = false;
+			let angleAcceleration = 1;
+			let angleForce = 0.05;
+
+			const setRotating = (state: boolean) => {
+				isRotating = state;
+
+				if (!state) {
+					angleAcceleration = 1;
+				}
+			};
+
+			onTouchStart(() => setRotating(true));
+			onMouseDown(() => setRotating(true));
+
+			onTouchEnd(() => setRotating(false));
+			onMouseRelease(() => setRotating(false));
+
+			car.onDraw(() => {
+				car.move(g.Vec2.fromAngle(car.angle).scale(car.speed));
+
+				if (isRotating) {
+					car.angle += Math.min(angleAcceleration, 10);
+					angleAcceleration += angleForce;
+				}
+			});
+
+			// Coin spawning
+			const coinSprite = await loadSprite('coin', '/coin.png');
+
+			function spawnCoin() {
+				const getDistance = (x1: number, y1: number, x2: number, y2: number) => {
+					var a = x1 - x2;
+					const b = y1 - y2;
+					const c = Math.sqrt( a*a + b*b );
+
+					return c;
+				}
+				const getPos = (iteration = 0): {x: number; y: number} => {
+					const randX = Math.random();
+					const randY = Math.random();
+
+					const maxX = g.width() - 400;
+					const maxY = g.height() - 400;
+
+					const x = 200 + Math.round(maxX * randX);
+					const y = 200 + Math.round(maxY * randY);
+
+					if(iteration >= 100) {
+						return {x,y};
+					}
+
+					if(getDistance(x,y, car.pos.x, car.pos.y) < 200) {
+						return getPos(iteration + 1);
+					}
+
+					return {x,y};
+				};
+
+
+				const coinPos = getPos();
+
+				add([
+					sprite('coin'),
+					pos(coinPos.x, coinPos.y),
+					area(),
+					gravity(0),
+					body(),
+					'coin',
+					g.origin('center')
+				]);
+			}
+
+			spawnCoin();
+
+			// Coin functionality
+			const onPickupCoin = () => {
+				car.speed += 10;
+				angleForce += 0.005;
+			};
+
+			car.onCollide("coin", (coin) => {
+				destroy(coin);
+				onPickupCoin();
+				setTimeout(() => {
+					spawnCoin();
+				}, 500);
+			});
+		});
+
+		go("game");
 	});
 </script>
 
